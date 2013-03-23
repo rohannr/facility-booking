e63eab7eddbce08d79046af2bc5f73e6f19c1a92
Rohan Ramakrishna <rohannr@gmail.com>
2013-03-24 02:33:13 +0800

    Client marshalls availability request,Server processes the received data

--- Client/src/com/distributed/client/Client.java
+++ Client/src/com/distributed/client/Client.java
@@ -1,29 +1,69 @@
 package com.distributed.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class Client {
 
+	String facName;
+	int service;
+	int UID;
+	
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException, ParseException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		System.out.println("Welcome to the Facility Booking System!\n" +
+				"You can access the following services:\n" +
+				"[1] Check Availability of Facilities\n" +
+				"[2] Book Facility\n" +
+				"[3] Change Booking\n" +
+				"[4] Monitor Facility Availability\n" +
+				"Please enter the corresponding number:");
+		
+		int choice = Integer.parseInt(br.readLine());
+		String msg = String.valueOf(choice);
+		switch(choice){
+		case 1: 
+			System.out.println("Enter name of facility followed by the required days: \n");
+			String line = br.readLine();
+			
+			
+			msg = msg + " " + line;
+			break;
+			
+		default:
+			;
+			
+		}
+		
 		
 		DatagramSocket sock = null;
 		try {
 			sock = new DatagramSocket();
-			byte[] m = args[0].getBytes();
 			InetAddress host = InetAddress.getByName(args[1]);
-			
+			byte[] m = msg.getBytes(); //Marshalling lol
 			int serverPort = 6789;
 			DatagramPacket request = new DatagramPacket(m, m.length, host, serverPort);
 			sock.send(request);
 			byte[] buffer = new byte[1000];
 			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-			sock.receive(reply);
 			System.out.println("Reply: " + new String(reply.getData()));
 			
 		} catch (Exception e) {
--- Server/src/com/distributed/server/UDPServer.java
+++ Server/src/com/distributed/server/UDPServer.java
@@ -2,6 +2,11 @@ package com.distributed.server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 
 public class UDPServer {
@@ -9,17 +14,37 @@ public class UDPServer {
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		
 		DatagramSocket sock = null;
+		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yy");
 		try {
 			sock = new DatagramSocket(6789);
 			byte[] buffer = new byte[1000];
 			while(true){
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
 				sock.receive(request);
-				System.out.println("Server received: " + request.getData());
+				String msg = new String(request.getData());
+				StringTokenizer tok = new StringTokenizer(msg);
+				int service = Integer.parseInt(tok.nextToken());
+				
+				switch(service){
+
+				case 1: 
+					String facName = tok.nextToken();
+					Vector<Date> daysToCheck = new Vector<Date>();
+					while(tok.hasMoreElements()){
+						daysToCheck.add((Date)df.parseObject(tok.nextToken()));
+					}
+					System.out.println(facName + " requested for " + daysToCheck);
+
+
+				default:
+					;
+				}
+				
 				DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
 				sock.send(reply);
 			}
