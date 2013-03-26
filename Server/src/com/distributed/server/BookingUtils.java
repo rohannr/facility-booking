167cce9b18526f4d3f51b2d5aaa38934f22d7b2c
Jason Wang <gnawnosaj@hotmail.com>
2013-03-26 18:17:02 +0100

    add key to date function to booking utils

--- Server/src/com/distributed/server/BookingUtils.java
+++ Server/src/com/distributed/server/BookingUtils.java
@@ -31,4 +31,27 @@ public class BookingUtils {
 			return -1;
 		}
 	}
+    
+    public static String getString(int day){
+        if (day == 0){
+            return "Monday";
+        } else if (day == 1) {
+            return "Tuesday";
+        } else if (day == 2) {
+            return "Wednesday";
+        } else if (day == 3) {
+            return "Thursday";
+        } else if (day == 4) {
+            return "Friday";
+        } else if (day == 5) {
+            return "Saturday";
+        } else if (day == 6) {
+            return "Sunday":
+        } else {
+            return "invalid"
+        }
+    }
+        
+        
+        
 }
